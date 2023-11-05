f5908d1907e4f677e65d31c540847b6dac9321c3
s-avery <s_avery@riseup.net>
2023-11-05 14:20:15 -0500

    took it out cuz bugging lol

--- src/index.js
+++ src/index.js
@@ -8,9 +8,7 @@ import AuthProvider from "./providers/AuthProvider";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<AuthProvider>
-			<App />
-		</AuthProvider>
+		<App />
 	</React.StrictMode>
 );
 
