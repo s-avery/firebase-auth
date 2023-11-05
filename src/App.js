97c8c5f8957d98fcc68d32e4239f8e041ada12fd
s-avery <s_avery@riseup.net>
2023-11-05 13:47:26 -0500

    signup card done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import LoginCard from "./Components/LoginCard";
+import SignupCard from "./Components/SignupCard";
 
 const App = () => {
 	return (
 		<div className="App">
 			slay
 			<LoginCard />
+			<SignupCard />
 		</div>
 	);
 };
--- src/Components/SignupCard.js
+++ src/Components/SignupCard.js
@@ -2,12 +2,39 @@ import { useState, useContext } from "react";
 import { AuthContext } from "../providers/AuthProvider";
 
 const SignupCard = () => {
-// !LOGIC
+	// !LOGIC
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
-	const { logIn } = useContext(AuthContext);
+	const { createNewUser } = useContext(AuthContext);
 
-
-	return <div>SignupCard</div>;
+	// !RETURN
+	return (
+		<div>
+			<form>
+				<h1>Create Account</h1>
+				<input
+					type="email"
+					placeholder="Enter your email"
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+				></input>
+				<input
+					type="password"
+					placeholder="Enter your password"
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+				></input>
+				<button
+					type="submit"
+					onClick={(e) => {
+						e.preventDefault();
+						createNewUser(email, password);
+					}}
+				>
+					Sign Up
+				</button>
+			</form>
+		</div>
+	);
 };
 export default SignupCard;
