41db020313fec9509bdc54bdce4dc6787565531b
s-avery <s_avery@riseup.net>
2023-11-05 14:15:39 -0500

    tested login with onSubmit; hardcoded login fn

--- src/Components/LogInCard.js
+++ src/Components/LogInCard.js
@@ -1,16 +1,32 @@
 import { useState, useContext } from "react";
-import { AuthContext } from "../providers/AuthProvider";
+// import { AuthContext } from "../providers/AuthProvider";
+import { auth } from "../Firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 const LoginCard = () => {
 	// !LOGIC
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
-	const { logIn } = useContext(AuthContext);
+	// const { logIn } = useContext(AuthContext);
+
+	const logIn = (loginEmail, loginPassword) => {
+		signInWithEmailAndPassword(auth, loginEmail, loginPassword)
+			// TODO: decide if we really want to do anything here
+			.then((userCredential) => {
+				console.log(userCredential);
+			})
+			.catch((err) => console.error(err));
+	};
 
 	// !RETURN
 	return (
 		<div>
-			<form onSubmit={logIn}>
+			<form
+				onSubmit={(e) => {
+					e.preventDefault();
+					logIn(email, password);
+				}}
+			>
 				<h1>Log In</h1>
 				<input
 					type="email"
